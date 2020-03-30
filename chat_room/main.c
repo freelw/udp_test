ea8650b96ae43c61d4725523d82ab2ce2985a5f0
liwang <liwang112358@gmail.com>
2020-03-30 23:40:26 +0800

    update

--- chat_room/main.c
+++ chat_room/main.c
@@ -5,7 +5,6 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     perror("no address set.");
@@ -39,10 +38,12 @@ int main(int argc, char *argv[]) {
       printf("Setting the local interface...OK\n");
     }
     while (1) {
+      char tmp[1024] = 0;
       memset(databuf, 0, datalen);
-      scanf("%s", databuf);
+      scanf("%s", tmp);
+      sprintf(databuf, "%s said : %s", localInterface, tmp);
       if (sendto(sd, databuf, datalen, 0, (struct sockaddr *)&groupSock,
-                sizeof(groupSock)) < 0) {
+                 sizeof(groupSock)) < 0) {
         perror("Sending datagram message error");
       } else {
         printf("Sending datagram message...OK\n");
@@ -51,6 +52,61 @@ int main(int argc, char *argv[]) {
     return 0;
   } else {
     printf("child process\n");
+
+    struct sockaddr_in localSock;
+    struct ip_mreq group;
+    char databuf[1024] = {0};
+    int datalen = sizeof(databuf);
+
+    int sd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sd < 0) {
+      perror("Opening datagram socket error");
+      exit(1);
+    } else {
+      printf("Opening datagram socket....OK.\n");
+    }
+      
+    int reuse = 1;
+    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse,
+                    sizeof(reuse)) < 0) {
+      perror("Setting SO_REUSEADDR error");
+      close(sd);
+      exit(1);
+    } else {
+      printf("Setting SO_REUSEADDR...OK.\n");
+    }
+    
+    memset((char *)&localSock, 0, sizeof(localSock));
+    localSock.sin_family = AF_INET;
+    localSock.sin_port = htons(4321);
+    localSock.sin_addr.s_addr = INADDR_ANY;
+    if (bind(sd, (struct sockaddr *)&localSock, sizeof(localSock))) {
+      perror("Binding datagram socket error");
+      close(sd);
+      exit(1);
+    } else {
+      printf("Binding datagram socket...OK.\n");
+    }
+
+    group.imr_multiaddr.s_addr = inet_addr("226.1.1.1");
+    group.imr_interface.s_addr = inet_addr(localInterfaceAddress);
+    if (setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&group,
+                   sizeof(group)) < 0) {
+      perror("Adding multicast group error");
+      close(sd);
+      exit(1);
+    } else {
+      printf("Adding multicast group...OK.\n");
+    }
+    while (1) {
+      if (read(sd, databuf, datalen) < 0) {
+        perror("Reading datagram message error");
+        close(sd);
+        exit(1);
+      } else {
+        printf("%s\n", databuf);
+      }
+    }
   }
   return 0;
 }
